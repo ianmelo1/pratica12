291fdcf750db1cf703c77f0359bfab8612892a3e
Caroline Lopes Martins <lpcarolinelopes@gmail.com>
2026-05-30 16:24:40 -0300

    feat: rotas configuradas e interface renderizando. closes #7

--- minha_aplicacao/src/App.jsx
+++ minha_aplicacao/src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Cadastro from './pages/Cadastro';
+import Listagem from './pages/Listagem';
+
 function App() {
   return (
-    <div>
-      <h1>Nebula rodando no React! 🌌</h1>
-    </div>
-  )
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/listagem" element={<Listagem />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ minha_aplicacao/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <main className="container">
+      <h1>Feed de Notícias</h1>
+      <div id="feed-lista">
+        <p className="loading">Carregando posts...</p> 
+      </div>
+    </main>
+  );
+}
+export default Home;
